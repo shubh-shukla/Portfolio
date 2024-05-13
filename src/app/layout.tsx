6dcd4b27fc2e1315f0f0caa3525e9648e9668f66
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-13 21:08:10 +0530

    app structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,93 @@
+import Script from 'next/script';
+import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
+
+import './globals.css';
+import Header from '@/components/layout/header';
+import { Providers } from '@/lib/providers';
+import Footer from '@/components/layout/footer';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const title = 'Shubham Shukla | Full Stack Developer From Bengaluru, India.';
+const description =
+  'A self-proclaimed designer who specializes in full stack development (MERN, NextJS, React Native), from Bengaluru, India.';
+const url = 'https://shubhamshukla.dev';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(url),
+  title,
+  description,
+  keywords: [
+    'Frontend Developer',
+    'Full Stack Developer',
+    'React Developer',
+    'Next.js Developer',
+  ],
+  creator: 'Shubham Shukla',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  openGraph: {
+    type: 'website',
+    url,
+    title,
+    description,
+    siteName: title,
+    images: [
+      {
+        url: '/images/open-graph-shubham.png',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    creator: '@shubh-shukla',
+    images: '/images/open-graph-shubham.png',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+};
+
+const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID;
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
+      {googleAnalyticsId ? (
+        <head>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          ></Script>
+          <Script id="google-anayltics-script">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${googleAnalyticsId}');
+          `}
+          </Script>
+        </head>
+      ) : null}
+      <body className={`${inter.className} bg-gray text-gray-600 antialiased`}>
+        <Providers>
+          <Header />
+          <main className="flex min-h-screen w-full flex-col">{children}</main>
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
