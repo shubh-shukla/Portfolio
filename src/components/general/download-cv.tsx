eaa22704c9b9f934fc94494e51a16dce95a91ff0
shubh-shukla <shukla111shubh@gmail.com>
2026-03-23 18:31:18 +0530

    Updated CV navigation

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import Button from '@/components/general/button';
+import Link from 'next/link';
+import { EXTERNAL_LINKS } from '@/lib/data';
 
 type DownloadCVProps = {
   className?: string;
@@ -10,9 +12,17 @@ const DownloadCV = ({ className }: DownloadCVProps) => {
   return (
     <Button
       className={className}
-      onClick={() => window?.open('/files/Shubham_Sr_Software_Eng_CV.pdf', '_blank')}
+      // onClick={() => window?.open('/files/Shubham_Sr_Software_Eng_CV.pdf', '_blank')}
+      asChild
     >
-      Download CV
+      <Link
+        href={EXTERNAL_LINKS.LINKEDIN}
+        target="_blank"
+        rel="noopener noreferrer"
+        prefetch={false}
+      >
+        Get My CV
+      </Link>
     </Button>
   );
 };
